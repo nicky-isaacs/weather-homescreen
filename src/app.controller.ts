63bf2840947ea88e46d93abfc148eed45de8d4e6
Nick Isaacs <nicholas.isaacs@icloud.com>
2022-09-21 20:31:46 -0600

    Added stream route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, Res, StreamableFile } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Readable } from 'stream';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/gif_data.gif')
+  @Header('Content-Type', 'image/gif')
+  async getGIFData() {
+    const body = await this.appService.getGIFData();
+
+    return new StreamableFile(new Readable().wrap(body));
+  }
 
   @Get()
   getHello() {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import { AxiosResponse } from 'axios';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getGIFData(): Promise<NodeJS.ReadableStream> {
+    const response: AxiosResponse<NodeJS.ReadableStream> = await firstValueFrom(
+      this.httpService.get<NodeJS.ReadableStream>(
+        'https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/taw/GEOCOLOR/GOES16-TAW-GEOCOLOR-900x540.gif',
+        { responseType: 'stream' },
+      ),
+    );
+    return response.data;
+  }
+
   getHello() {
     return {
       data: 'good fucking data right here',
