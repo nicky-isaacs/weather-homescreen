8a0c74b904a55ca67edc02178aaed89ecf82492c
Nick Isaacs <nicholas.isaacs@icloud.com>
2022-10-30 16:50:46 -0600

    log headers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header, StreamableFile } from '@nestjs/common';
+import { Controller, Get, Header, Req, StreamableFile } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Readable } from 'stream';
 
@@ -8,7 +8,8 @@ export class AppController {
 
   @Get('/image_data.tif')
   @Header('Content-Type', 'image/tif')
-  async getGIFData() {
+  async getGIFData(@Req() request: Request) {
+    console.log('headers', JSON.stringify(request.headers));
     const body = await this.appService.getTIFData();
 
     return new StreamableFile(new Readable().wrap(body));
